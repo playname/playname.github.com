d71fcd5bede21b4770c1232c99ac696c6746b02d
Playname <obruhns03@gmail.com>
2017-04-11 00:25:10 +0200

    Made the editor usable

--- editor/editor.js
+++ editor/editor.js
@@ -1,4 +1,21 @@
-function run() {
-	$("#window").html($("#code").text());
-	Prism.highlightAll();
+var h = false;
+var code;
+
+function run(code) { $("#window").html(code); }
+
+function highlight() {
+	if (!h) {
+		code = $("#code").val();
+		$("#code").replaceWith("<pre id='cc'><code class='language-html' id='hcode'></code></pre>");
+		$("#cc").prop("style", "width: 47vw; height: 89vh;");
+		$("#hcode").text(code);
+		$("#highlight").text("Edit");
+		Prism.highlightAll();
+		h = true;
+	} else {
+		$("#cc").replaceWith("<textarea id='code'></textarea>");
+		$("#code").text(code);
+		$("#highlight").text("Highlight");
+		h = false;
+	}
 }
