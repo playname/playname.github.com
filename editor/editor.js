73675dbfb146b41a64842c058ffce9069c049bf6
Playname <obruhns03@gmail.com>
2017-04-11 18:48:39 +0200

    Fixed editor not working before clicking Highlight

--- editor/editor.js
+++ editor/editor.js
@@ -1,13 +1,18 @@
 var h = false;
 var code;
 
-function run(code) { $("#window").html(code); }
+function run() {
+	if (h)
+		$("#window").html(code);
+	else
+		$("#window").html($("#code").val());
+}
 
 function highlight() {
 	if (!h) {
 		code = $("#code").val();
 		$("#code").replaceWith("<pre id='cc'><code class='language-html' id='hcode'></code></pre>");
-		$("#cc").prop("style", "width: 47vw; height: 89vh;");
+		$("#cc").prop("style", "width: 46vw; height: 86vh;");
 		$("#hcode").text(code);
 		$("#highlight").text("Edit");
 		Prism.highlightAll();
