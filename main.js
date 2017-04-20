d9f7ebcd2bf8432c237db83c605de70b1ad0412e
Playname <obruhns03@gmail.com>
2017-04-20 18:22:20 +0200

    Change background image

--- main.js
+++ main.js
@@ -1,14 +1,36 @@
 /*function showHint(text, parent) { $("#"+parent).append("<div id='"+parent+"hint'>"+text+""); }
 function hideHint(hintId) { $("#"+hintId).remove(); }*/
-var repeat = true;
+
+//Konami code
+var k = true;
 var stage = 0;
 var pressed = false;
 
-window.onload = konami();
+window.onload = main();
+
+function main() {
+  requestAnimationFrame(main);
+
+  update();
+  if (k) konami();
+}
+
+function update() {
+  //TODO: debug, settings, other update stuff
+}
+
+function changeBg() {
+  var file = $("#bg")[0].files[0];
+  var reader = new FileReader();
+  reader.onloadend = function(){
+    $("body").css({"background-image":"url(" + reader.result + ")", "background-size":"cover"});
+  }
+  if(file){
+    reader.readAsDataURL(file);
+  }
+}
 
 function konami() {
-  if (repeat) requestAnimationFrame(konami);
-
   $(document).keydown(function(e) {
     if (!pressed) {
       if (e.which == 38 && stage <= 2) stage++;
@@ -19,7 +41,7 @@ function konami() {
       else if (e.which == 39 && stage == 7) stage++;
       else if (e.which == 66 && stage == 8) stage++;
       else if (e.which == 65 && stage == 9) stage++;
-      else if (e.which == 13 && stage == 10) {repeat = false; replace();}
+      else if (e.which == 13 && stage == 10) {k = false; replace();}
       else stage = 0;
       pressed = true;
     }
