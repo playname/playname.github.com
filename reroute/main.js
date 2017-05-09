0651f1ad8003674273089508809b362211fb499c
Playname <obruhns03@gmail.com>
2017-05-09 19:35:27 +0200

    I think I fixed it

--- reroute/main.js
+++ reroute/main.js
@@ -1,18 +1,3 @@
-/*function generate() {
-  var url = $("#in").val();
-  var link = "https://playname.github.io/rr/?l=";
-  var tmp = new Array(url.length);
-
-  for (var i = 0; i < url.length; i++) {
-    var t = -(~url.charCodeAt(i));
-    tmp[i] = String.fromCharCode(t);
-  }
-  tmp = tmp.join("");
-
-  link = link + tmp;
-  $("#out").text(link);
-}*/
-
 function generate() {
   var url = $("#in").val();
   var link = "https://playname.github.io/rr/?l=";
